6a5d846b5670451279e84a2977f3a409648db72e
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-17 01:03:09 +0100

    feat: user validation to only add users with unique emails

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return;
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException('This user already exists with that email');
+    }
   }
 }
